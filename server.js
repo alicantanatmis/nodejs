c99f031c057d35411731900d2b52332ad404d2ea
Alican Tanatmis <alicantan60@gmail.com>
2014-12-13 16:24:51 -0500

    Changing database connection to use openshift online database

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var ipaddr  = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port    = process.env.OPENSHIFT_NODEJS_PORT || 80;
 
 var connection = mysql.createConnection({
-user: "root",
-password: "password",
+user: "adminuvegAVd",
+password: "DFMZhg363W8p",
 database: "tut"
 });
 
